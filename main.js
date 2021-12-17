37a87bfdd823d6a50c03dc015acc9b987ef521c9
GildsonFaria1990 <89483870+GildsonFaria1990@users.noreply.github.com>
2021-12-17 16:01:54 -0300

    Cra-chá

---
+++ main.js
@@ -0,0 +1,34 @@
+const LinksSocialMedia = {
+  github: 'GildsonFaria1990',
+  youtube: 'results?search_query=gildson+faria-93hyJA',
+  facebook: 'gildson.faria.9',
+  instagram: 'gildsonfaria',
+  twitter: 'GildsonFaria'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href
+    ;('https://${social}.com/${LinksSocialMedia[social]}')
+
+    //  alert(li.children[0].href);
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = 'https://api.github.com/users/${LinksSocialMedia.github}'
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
